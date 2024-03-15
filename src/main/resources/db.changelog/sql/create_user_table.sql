769cc1d3e7ce22027a0406d44028768b97cb1350
Ilya Kuznetsov <ilyakuznecov84@gmail.com>
2024-03-15 15:57:56 +0300

    Added liquibase and build ci/cd

---
+++ src/main/resources/db.changelog/sql/create_user_table.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset EuphoriaV:create_table_user
+
+create table wechat.user
+(
+    id          bigserial primary key,
+    login       varchar unique not null,
+    name        varchar        not null,
+    created_at  timestamp with time zone,
+    modified_at timestamp with time zone
+);
+
+--rollback drop table wechat.user;
